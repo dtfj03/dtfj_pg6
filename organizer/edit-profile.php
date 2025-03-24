77814abfb04c44c989f3876c3de14e1fea5be6e0
Dante Filasol Jr. <142869485+dtfj03@users.noreply.github.com>
2025-03-24 08:14:40 +0800

    istg pls work

--- organizer/edit-profile.php
+++ organizer/edit-profile.php
@@ -1,6 +1,24 @@
 <?php
-session_start();
-include('fetch-organizer.php'); // Fetch user data
+//config.php
+$servername = "localhost:3306";
+$username = "dtfj_pg6";
+$password = "mobiledev";
+$database = "dtfj_pg6";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+
+include('../organizer/verify-org.php');
+
+$org_id = $_SESSION['org_id'];
+
+$result = $conn->query("SELECT * FROM organizer WHERE org_id='$org_id'");
+
 ?>
 
 <!DOCTYPE html>
